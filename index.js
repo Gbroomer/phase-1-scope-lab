c8251f402c8fdac8d3ccd4a35874cdc81777c011
Grant Bruemmer <grantbruemmer@gmail.com>
2023-06-13 12:26:16 -0500

    complete Scope Lab 1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+//var customerName = 'bob';
+//upperCaseCustomerName = () => customerName.toUpperCase();
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+setBestCustomer = () => (bestCustomer = 'not bob');
+overwriteBestCustomer = () => (bestCustomer = 'maybe bob');
+const leastFavoriteCustomer = 'Probably Bob...';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Definitely Bob!';
+}
